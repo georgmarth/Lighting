81f1a5d8650b48815e6545e2cd01ea26fb9d2858
Georg Marth <marth.georg@gmail.com>
2019-05-07 15:39:41 +0200

    updated lighting

---
+++ Assets/RotateLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour
+{
+
+    public float rotationSpeed = 1f;
+    
+    void Update()
+    {
+        Vector3 oldRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(
+            oldRotation.x,
+            oldRotation.y + rotationSpeed * Time.deltaTime,
+            oldRotation.z
+            );
+    }
+}
